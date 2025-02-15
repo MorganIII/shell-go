404fb9428df803b548dcbf328d10bf54dc878861
MorganIII <mosaab.morgan@gmail.com>
2025-02-15 17:49:55 +0200

    [any message]

--- cmd/myshell/command.go
+++ cmd/myshell/command.go
@@ -29,10 +29,12 @@ func (c Command) handleCommand() {
 		fmt.Println(echoed)
 	case "type":
 		command := c.Args[0]
-		if _, ok := shellCommands[command]; !ok {
-			fmt.Fprintf(os.Stderr, "%s: not found\n", command)
-		} else {
+		if _, ok := shellCommands[command]; ok {
 			fmt.Printf("%s is a shell builtin\n", command)
+		} else if path := c.lookupCommand(); path != "" {
+			fmt.Printf("%s is %s\n", c.Args[0], path)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s: not found\n", command)
 		}
 	default:
 		fmt.Fprintf(os.Stderr, "%s: command not found\n", strings.TrimSpace(c.Name))
@@ -43,4 +45,21 @@ func (c *Command) trimArgSpaces() {
 	for i := range c.Args {
 		c.Args[i] = strings.TrimSpace(c.Args[i])
 	}
+}
+
+func (c Command) lookupCommand() string {
+	path := os.Getenv("PATH")
+	existPaths := strings.Split(path, string(os.PathListSeparator))
+	for i := len(existPaths) - 1; i >= 0; i-- {
+		entries, err := os.ReadDir(existPaths[i])
+		if err != nil {
+			continue
+		}
+		for _, e := range entries {
+			if !e.Type().IsDir() && e.Name() == c.Args[0] {
+				return existPaths[i] + string(os.PathSeparator) + e.Name()
+			}
+		}
+	}
+	return ""
 }
